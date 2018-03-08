7a1c4a2608190601c621496b4005539d7bb0cb6a
Hiragi1585 <hiragi.gkuth@gmail.com>
2018-03-08 16:19:49 +0900

    add django project

---
+++ web/blog/views.py
@@ -0,0 +1,10 @@
+from django.shortcuts import render
+from django.utils import timezone as tz
+from .models import Post
+
+
+# Create your views here.
+def post_list(request):
+    posts = Post.objects.filter(published_date__lte=tz.now()).order_by("published_date")
+    return render(request,'blog/post_list.html',{'posts':posts})
+
