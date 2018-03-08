090e613986784e25b018e7c437f46d88eb248bb5
Hiragi1585 <hiragi.gkuth@gmail.com>
2018-03-08 23:44:53 +0900

    complete beacon system

--- web/ss/views.py
+++ web/ss/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Beacon,Event,User
+
 
 # Create your views here.
 
@@ -18,6 +20,17 @@ def receive(request):
         "timestamp":request.POST["timestamp"],
     }
     
-    for key,value in db_param.items():
-        print(key + ":" + value)
+    data = Beacon()
+    data.user_id = db_param["user_id"]
+    data.hw_id = db_param["hw_id"]
+    data.timestamp = db_param["timestamp"]
+    
+    data.save()
+    
+    print("DB_Inserted")
+    print(Beacon.objects.all())
+    
+    owner = User.objects.filter(beacon_id=db_param["hw_id"])
+    print(owner)
+    
     return HttpResponse("Data received")
